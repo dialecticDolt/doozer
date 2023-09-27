e19783ed37d20848ab3b19e9024f6e4bdc6d979a
dialecticDolt <will.ruys@gmail.com>
2023-09-27 17:02:39 -0500

    TEST: Added explicit test for estimate_frequency

--- test/common_gpu.py
+++ test/common_gpu.py
@@ -7,6 +7,27 @@ import numpy as np
 from common import verify_time
 
 
+def verify_sleep_cycles(sleep_func: Callable, args, microseconds: int, samples: int = 10) -> bool:
+    assert (cp.cuda.runtime.getDevice() == 0)
+
+    stream = cp.cuda.get_current_stream()
+    sleep_func(0, *args, stream)
+    stream.synchronize()
+
+    times = []
+    for l in range(samples):
+        start_t = time.time()
+        sleep_func(0, *args, stream)
+        stream.synchronize()
+        end_t = time.time()
+        times.append(end_t - start_t)
+    elapsed = np.mean(np.asarray(times))
+
+    print(
+        f"Time taken: {elapsed} seconds. Expected: {microseconds / 1e6} seconds.")
+    return verify_time(elapsed, microseconds / 1e6)
+
+
 def verify_sleep(sleep_func: Callable, args, microseconds: int, samples: int = 10) -> bool:
     assert (cp.cuda.runtime.getDevice() == 0)
 
@@ -23,6 +44,32 @@ def verify_sleep(sleep_func: Callable, args, microseconds: int, samples: int = 1
         times.append(end_t - start_t)
     elapsed = np.mean(np.asarray(times))
 
+    print(
+        f"Time taken: {elapsed} seconds. Expected: {microseconds / 1e6} seconds.")
+    return verify_time(elapsed, microseconds / 1e6)
+
+
+def verify_sleep_cycles_event(sleep_func: Callable, args, microseconds: int, samples: int = 10) -> bool:
+    assert (cp.cuda.runtime.getDevice() == 0)
+
+    stream = cp.cuda.get_current_stream()
+    sleep_func(0, *args, stream)
+    stream.synchronize()
+
+    times = []
+    for l in range(samples):
+        start_event = cp.cuda.Event()
+        end_event = cp.cuda.Event()
+
+        start_event.record(stream)
+        sleep_func(0, *args, stream)
+        end_event.record(stream)
+        stream.synchronize()
+        elapsed = cp.cuda.get_elapsed_time(
+            start_event, end_event)/1000
+        times.append(elapsed)
+
+    elapsed = np.mean(np.asarray(times))
     print(
         f"Time taken: {elapsed} seconds. Expected: {microseconds / 1e6} seconds.")
     return verify_time(elapsed, microseconds / 1e6)
--- test/test_gpu_bsleep.py
+++ test/test_gpu_bsleep.py
@@ -7,17 +7,25 @@ from doozer.gpu import estimate_frequency
 
 # , sleep_with_gil, estimate_frequency
 
-from common_gpu import verify_sleep, verify_sleep_event
+from common_gpu import verify_sleep, verify_sleep_event, verify_sleep_cycles, verify_sleep_cycles_event
 
 import cupy as cp
 
 CYCLES = 1900000000
 
 
-# def test_estimate_frequency():
-#     cycles = estimate_frequency(
-#         sleep_cycles, target_time=5000, use_event=False)
-#     print(cycles)
+@pytest.mark.parametrize("us", [500, 1000, 2000, 5000, 10000, 20000, 50000])
+def test_estimate_frequency(us: int):
+    cycles = estimate_frequency(sleep_cycles, target_time=us, use_event=False)
+    status = verify_sleep_cycles(sleep_cycles, (cycles,), us)
+    assert status
+
+
+@pytest.mark.parametrize("us", [500, 1000, 2000, 5000, 10000, 20000, 50000])
+def test_estimate_frequency_event(us: int):
+    cycles = estimate_frequency(sleep_cycles, target_time=us, use_event=True)
+    status = verify_sleep_cycles_event(sleep_cycles, (cycles,), us)
+    assert status
 
 
 @pytest.mark.parametrize("us", [500, 1000, 2000, 5000, 10000, 20000, 50000])
